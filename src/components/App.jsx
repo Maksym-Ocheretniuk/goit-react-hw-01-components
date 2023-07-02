e9b2d1eb44a6d2dd4ff4cba50ae90d5c27eb7f7d
Maksym Ocheretniuk <max.ocheret@gmail.com>
2023-07-03 01:51:09 +0300

    Profile

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,29 @@
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+
+import { Profile } from './Profile';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Profile />
+    </>
   );
 };
+
+// export const App = () => {
+//   return (
+//     <div
+//       style={{
+//         height: '100vh',
+//         display: 'flex',
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         fontSize: 40,
+//         color: '#010101'
+//       }}
+//     >
+//       React homework template
+//     </div>
+//   );
+// };
